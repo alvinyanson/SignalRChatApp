a797035d1a33c956917bc9ac755c7cf78092c5e5
alvinyanson <a.yanson@arcanys.com>
2024-09-03 18:35:21 +0800

    Add project files.

---
+++ SignalRChatApp/SignalRChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalRChatApp.Hubs
+{
+    public class ChatHub : Hub<IChatHub>
+    {
+        //public override async Task OnConnectedAsync()
+        //{
+        //    await Clients.Others.SuccessSendMessage(Context.ConnectionId, $"joined the party!");
+        //    await base.OnConnectedAsync();
+        //}
+
+        //public override async Task OnDisconnectedAsync(Exception? exception)
+        //{
+        //    await Clients.Others.SuccessSendMessage(Context.ConnectionId, $"left the party!");
+        //}
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SuccessSendMessage(user, message);
+        }
+
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SuccessSendMessage(Context.ConnectionId, $"joined the {groupName}!");
+        }
+
+        public async Task SendMessageToGroup(string groupName, string message)
+        {
+            await Clients.Group(groupName).SuccessSendMessage(Context.ConnectionId, $"{message}");
+        }
+    }
+}
---
+++ SignalRChatApp/SignalRChatApp/Hubs/IChatHub.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorSignalRChatApp.Hubs
+{
+    public interface IChatHub
+    {
+        Task SuccessSendMessage(string user, string message);
+    }
+}
